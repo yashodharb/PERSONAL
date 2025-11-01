2446ea0d02e3cf1ece29e59cfb74d7723eb5ad98
Yashodhar Bhosale <yashbhosale55@gmail.com>
2025-11-01 05:57:34 +0530

    dll updated

--- Code/Projects/C/DSA/doubleLinkedList/dll.c
+++ Code/Projects/C/DSA/doubleLinkedList/dll.c
@@ -1,48 +1,90 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-typedef struct node
-{
+typedef struct node {
     int data;
     struct node *next;
     struct node *prev;
-}node;
+} node;
 
-node *head = NULL;              /* Will point to the head always */
-node *tail = NULL;              /* Will point to the tail always */
-node *current = NULL;           /* Will be used to traverse the dll */
-
-node *createNode(int data)
-{
-    node *initial_node = (node *)malloc(sizeof(node));
-    if(initial_node==NULL){
-        printf("Not enough memory\n");
+node* createNode(int data) {
+    node* newNode = (node*)malloc(sizeof(node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
         exit(1);
     }
-    initial_node->data = data;
-    initial_node->prev = NULL;
-    initial_node->next = NULL;
-    return initial_node;
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void insertAtHead(node** head_ref, int data) {
+    node* newHeadNode = createNode(data);
+    newHeadNode->next = *head_ref;
+    newHeadNode->prev = NULL;
+
+    if (*head_ref != NULL)
+        (*head_ref)->prev = newHeadNode;
+
+    *head_ref = newHeadNode;
+}
+
+void insertAtTail(node** head_ref, int data) {
+    node* tailNode = createNode(data);
+
+    if (*head_ref == NULL) {
+        *head_ref = tailNode;
+        return;
+    }
+
+    node* tmp = *head_ref;
+    while (tmp->next != NULL)
+        tmp = tmp->next;
+
+    tmp->next = tailNode;
+    tailNode->prev = tmp;
+}
+
+node* search(node* head, int key) {
+    node* temp = head;
+    while (temp != NULL) {
+        if (temp->data == key)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+void deleteNode(node** head_ref, node* del) {
+    if (*head_ref == NULL || del == NULL)
+        return;
+
+    if (*head_ref == del)
+        *head_ref = del->next;
+
+    if (del->next != NULL)
+        del->next->prev = del->prev;
+
+    if (del->prev != NULL)
+        del->prev->next = del->next;
+
+    free(del);
 }
 
 void printDll(node* head) {
     node* temp = head;
     printf("Forward Traversal: ");
- while (temp != NULL) {
-        printf("%d ", temp->data); // Print the data in each node
-        if (temp->next == NULL) {
-            break;  // Stop when we reach the last node
-        }
-        temp = temp->next;  // Move to the next node
-    }
-    printf("\n");
-
-    // Call the reverse traversal function starting from the last node
-    printf("Reverse Traversal: ");
     while (temp != NULL) {
-        printf("%d ", temp->data); // Print the data in each node
-        temp = temp->prev;         // Move to the previous node
+        printf("%d ", temp->data);
+        if (temp->next == NULL)
+            break;
+        temp = temp->next;
+    }
+    printf("\nReverse Traversal: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->prev;
     }
     printf("\n");
 }
@@ -50,30 +92,29 @@ void printDll(node* head) {
 void freeList(node* head) {
     node* temp;
     while (head != NULL) {
-        temp = head;         // Store the current node
-        head = head->next;   // Move to the next node
-        free(temp);          // Free the current node
+        temp = head;
+        head = head->next;
+        free(temp);
     }
 }
 
-int main(void)
-{
-        // Create nodes
-    node* node1 = createNode(10);  // First node
-    node* node2 = createNode(20);  // Second node
-    node* node3 = createNode(30);  // Third node
+int main(void) {
+    node* head = NULL;
 
-    // Link nodes
-    node1->next = node2;
-    node2->prev = node1;
-    node2->next = node3;
-    node3->prev = node2;
+    insertAtHead(&head, 10);
+    insertAtHead(&head, 20);
+    insertAtTail(&head, 5);
+    insertAtTail(&head, 1);
 
-    // Print the list with forward and reverse traversal
-    printDll(node1);
+    printDll(head);
 
-    // Free allocated memory
-    freeList(node1);
-    
+    node* n = search(head, 10);
+    if (n)
+        deleteNode(&head, n);
+
+    printf("After deleting 10:\n");
+    printDll(head);
+
+    freeList(head);
     return 0;
 }
